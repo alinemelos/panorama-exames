e192c0d3fbd390bc5b0094b69effc90d1fde6426
italown <italo.leandro25@gmail.com>
2026-06-17 10:07:42 -0300

    fix: ajuste no bug de login cacheado

--- backend/apps/authentication/authentication.py
+++ backend/apps/authentication/authentication.py
@@ -1,10 +1,14 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.exceptions import AuthenticationFailed, InvalidToken, TokenError
 
 
 class CookieJWTAuthentication(JWTAuthentication):
     def authenticate(self, request):
+        raw_token = request.COOKIES.get('access_token')
+        if raw_token is None:
+            return None
         try:
-            return super().authenticate(request)
-        except (InvalidToken, TokenError):
+            validated_token = self.get_validated_token(raw_token)
+            return self.get_user(validated_token), validated_token
+        except (InvalidToken, TokenError, AuthenticationFailed):
             return None
